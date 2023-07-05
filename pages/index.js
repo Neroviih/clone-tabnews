d51560e8c44a536954c662ddd9b4e6f19411e6cb
Neroviih <109556645+Neroviih@users.noreply.github.com>
2023-07-05 00:41:06 +0000

    mudando a frase

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
 function home(){
-    return <h1>Eu te amo meu bem, obrigado por estar sempre comigo, nos momentos dificeis e mais felizes também, Te amoo muito meu paozao.</h1>
+    return <h1>Eu te amo meu bem, obrigado por estar sempre comigo, nos momentos dificeis e mais felizes também, Te amoo muito meu paozao.s2s2s2ss22</h1>
 }
 export default home;
