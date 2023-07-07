e5b2085048eb4813d87db4c5f031d7b79d0aaf1e
Gabriel Sales <109556645+Neroviih@users.noreply.github.com>
2023-07-07 05:06:45 -0300

    Alterado a formatação do codigo

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
-function home(){
-    return <h1>Eu te amo meu bem, obrigado por estar sempre comigo, nos momentos dificeis e mais felizes também, Te amoo muito obriada 
-        por existir na minha vida nenem
+function home() {
+  return (
+    <h1>
+      Eu te amo meu bem, obrigado por estar sempre comigo, nos momentos dificeis
+      e mais felizes também, Te amoo muito obriada por existir na minha vida
+      nenem
     </h1>
+  );
 }
 export default home;
